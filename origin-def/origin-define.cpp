b840640017e660ab3e369a96f76f3030064a3ee2
BlinB209 <3200402001@qq.com>
2026-08-25 01:03:33 +0800

    Initial commit: 题目文件

---
+++ origin-def/origin-define.cpp
@@ -0,0 +1,37 @@
+/*------BlinB------*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+#define fullhead int main() {untie;
+#define multitesthead int TCOG = 0; cin >> TCOG; for(ll TNOG = 0; TNOG < TCOG; TNOG++) {
+#define untie ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define multitesttail }
+#define tail cout << flush; return 0;}
+#define mult fullhead multitesthead solve();multitesttail tail
+#define single fullhead solve(); tail
+#define vs void solve()
+#define fsp fixed << setprecision
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define pq priority_queue
+#define strm stringstream
+#define psb push_back
+#define ppb pop_back
+#define vc vector
+#define lt length
+#define ept empty
+#define endl '\n'
+#define bg begin
+#define ps push
+#define pk top
+#define pp pop
+#define INF 9e18
+#define PI 3.1415926535897932384626535897932384626
+
+vs{
+    cout<<1<<endl;
+}
+mult
+//single
